50df18ccaf714fb5268212f95a49048edb755518
Mark Rothermal <rothermal2@comcast.net>
2015-11-10 17:35:38 -0600

    added xtra cred.need to move it to its own repo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("this is only a text, i mean test.");
+
+
+var one = function (x){
+	return x*x;
+}
+var two = function (y){
+return y*y;
+}
+var three = function (z){
+return z*z;
+}
+
+console.log(one(two(three(2))));
