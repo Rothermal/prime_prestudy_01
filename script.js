b147abd4690ff1c04c54be9523b75167e35ba4e2
Mark Rothermal <rothermal2@comcast.net>
2015-11-13 15:18:35 -0600

    removed bonus challenge

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 console.log("this is only a text, i mean test.");
 
 
-var one = function (x){
-	return x*x;
-}
-var two = function (y){
-return y*y;
-}
-var three = function (z){
-return z*z;
-}
-
-console.log(one(two(three(2))));
